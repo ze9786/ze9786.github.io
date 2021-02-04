4d0af2e4db3c851baa16d640a55c91075b7aafa1
Zelina <zelinato@vgt-hk.net>
2021-02-04 17:59:06 +0800

    doing pw mask

--- password.js
+++ password.js
@@ -1,29 +1,11 @@
-var theButton = document.getElementById("alert");
-
-theButton.onclick = function () {
-    var thePrompt = window.prompt("Please enter password", "");
-    var theHTML = "";
-
-    // theHTML += "<p>The server http://localhost:8888 requires a username and password. The server says: These are restricted files, please authenticate yourself.</p>";
-    // theHTML += "<br/>";
-    // theHTML += "Username: <input type='text' id='theUser' placeholder='Enter Username...'/>";
-    // theHTML += "<br />";
-    theHTML += "Password: <input type='password' id='thePass' placeholder='Enter Password...'/>";
-    theHTML += "<br />";
-    theHTML += "<input type='button' value='OK' id='authOK'/>";
-    thePrompt.document.body.innerHTML = theHTML;
-
-    var thePass = thePrompt.document.getElementById("thePass").value;
-    thePrompt.document.getElementById("authOK").onclick = function () {
-        doAuthentication(thePass);
-    }
-}
-
-function doAuthentication(pass) {
-    //do authentication
-    if(pass=="1234"){
-        window.location.href="www.zelinato.com";
-    }else{
-        window.location.href="www.zelinato.com";
-    }
-}
+var password = prompt("Enter the password").;
+        if (password=="9786") {
+            //location = "media.html"
+            window.location.href="https://zelinato.com/about.html";
+        }
+        else 
+        {
+        //   document.getElementById("msg").value = "test";  
+        alert("Wrong password! Please contact admin if you want to access on this page.");
+          window.location.href="https://zelinato.com";
+        }
